c690436e2abbac3b9bb7e295fd65712d8e3fcde6
Laura McGowan <prime.lmcg@gmail.com>
2023-07-25 14:32:05 -0500

    static form written and working

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Form } from "react-router-dom";
+
 // * Array of objects
 const initialFriends = [
   {
@@ -25,7 +27,10 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -61,7 +66,51 @@ function Friend({ friend }) {
       )}
 
       {friend.balance === 0 && <p>You and {friend.name} are even.</p>}
-      <button className="button">Select</button>
+
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👬 Friend's name</label>
+      <input type="text" />
+
+      <label>🎆 Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💰 Bill value</label>
+      <input type="text" />
+
+      <label>🧍‍♂️Your expense</label>
+      <input type="text" />
+
+      <label>👬 X's expense</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">Friend</option>
+      </select>
+
+      <Button>Split Bill</Button>
+    </form>
   );
 }
